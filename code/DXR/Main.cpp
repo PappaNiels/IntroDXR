88bc0243b5d544d0f443db1ec88bd583709fd3cd
PappaNiels <pindakaastosti111@gmail.com>
2025-11-06 01:37:03 +0100

    feat: fps counter

--- code/DXR/Main.cpp
+++ code/DXR/Main.cpp
@@ -3,6 +3,8 @@
 #include <Utils/CLI.hpp>
 #include <Renderer/Renderer.hpp>
 
+#include <chrono>
+
 extern Renderer* CreateSample();
 
 void CreateConsole()
@@ -40,16 +42,41 @@ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE, LPSTR cmdLine, INT cmdShow)
 	Renderer* renderer = CreateSample();
 	renderer->Initialize();
 
+	auto oldTime = std::chrono::high_resolution_clock::now();
+	float elapsedTime = 0.0f;
+	uint32_t frameCount = 0;
+
 	MSG msg = {};
 	while (msg.message != WM_QUIT)
 	{
+		auto newTime = std::chrono::high_resolution_clock::now();
+		auto delta = newTime - oldTime;
+		auto dT = static_cast<float>(delta.count()) * 1.0e-9f;
+		oldTime = newTime;
+
+		elapsedTime += dT;
+
+		if (elapsedTime > 1.0f)
+		{
+			char buffer[128];
+
+			sprintf_s(buffer, "FPS: %f\n", frameCount / elapsedTime);
+			OutputDebugStringA(buffer);
+
+			elapsedTime = 0.0f;
+			frameCount = 0;
+		}
+
 		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
 
+		renderer->Update(dT);
 		renderer->Render();
+
+		frameCount++;
 	}
 
 	if (GetCLI().Console)
