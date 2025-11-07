7aa02854156247c73f148114c5acf4e9b90adfb6
PappaNiels <pindakaastosti111@gmail.com>
2025-11-07 01:27:19 +0100

    fix: assert formatting

--- code/DXR/Utils/Assert.hpp
+++ code/DXR/Utils/Assert.hpp
@@ -5,7 +5,7 @@
 void _Assert(bool expression, const std::string_view fmt, const std::string_view file, int line, ...);
 
 #if defined _DEBUG
-#define ASSERT(expression, fmt, ...) _Assert(expression, fmt, __FILE__, __LINE__ ##__VA_ARGS__)
+#define ASSERT(expression, fmt, ...) _Assert(expression, fmt, __FILE__, __LINE__, ##__VA_ARGS__)
 #else
 #define ASSERT(expression, fmt, ...) (void)(expression)
 #endif
